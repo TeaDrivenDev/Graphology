325d050570a8da8048d8b5b5b30e133cd8a241ac
atriesch <a.triesch@global-concepts.de>
2014-01-30 01:24:53 +0100

    InvalidCastExceptions in DefaultGetObjectFields ignored - this is not good

--- Src/Graphology/Traversal/DefaultGetObjectFields.cs
+++ Src/Graphology/Traversal/DefaultGetObjectFields.cs
@@ -33,12 +33,29 @@ namespace TeaDriven.Graphology.Traversal
 
             IEnumerable<FieldInfo> fields = this._getTypeFields.For(type);
 
-            IEnumerable<ObjectField> fieldValues = fields.Select(fi => new ObjectField()
-                                                                       {
-                                                                           ReferenceType = fi.FieldType,
-                                                                           ReferenceName = fi.Name,
-                                                                           FieldValue = currentObject.GetFieldValue(fi.Name)
-                                                                       });
+            IEnumerable<ObjectField> fieldValues = fields.Select(fi =>
+                                                                 {
+                                                                     ObjectField objectField;
+
+                                                                     try
+                                                                     {
+                                                                         objectField = new ObjectField()
+                                                                                       {
+                                                                                           ReferenceType = fi.FieldType,
+                                                                                           ReferenceName = fi.Name,
+                                                                                           FieldValue = currentObject.GetFieldValue(fi.Name)
+                                                                                       };
+                                                                     }
+                                                                     catch (InvalidCastException)
+                                                                     {
+                                                                         // FIXME: It's not good to just swallow these errors
+                                                                         // Many occurrences probably point to a problem with
+                                                                         // the type exclusion mechanism and should be examined
+                                                                         objectField = new ObjectField();
+                                                                     }
+
+                                                                     return objectField;
+                                                                 });
 
             return fieldValues;
         }
