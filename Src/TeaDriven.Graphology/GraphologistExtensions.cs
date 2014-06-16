50c5192f4cc8de9eb61aced1c51504b78391e625
dduerrenberger <d.duerrenberger@global-concept.de>
2014-06-16 17:12:47 +0200

    Beautified

--- Src/TeaDriven.Graphology/GraphologistExtensions.cs
+++ Src/TeaDriven.Graphology/GraphologistExtensions.cs
@@ -19,29 +19,25 @@ namespace TeaDriven.Graphology
         public static void DrawXmind(this IGraphologist graphologist, object targetObject, string projectPath, string graphName)
         {
             IGraphTraversal graphTraversal = new DefaultGraphTraversal();
-
             var graphRootNode = graphTraversal.Traverse(targetObject);
 
             var workbook = new XMindWorkBook(projectPath + @"\" + graphName + ".xmind");
-
             var sheetId = workbook.AddSheet("Graphology");
             var central = workbook.AddCentralTopic(sheetId, "Main", XMindStructure.Map);
 
-            DrawX(workbook, central, graphRootNode);
+            AddXMindTopics(workbook, central, graphRootNode);
 
             workbook.Save();
-
         }
 
-        private static void DrawX(XMindWorkBook workbook, string parentId, GraphNode node)
+        private static void AddXMindTopics(XMindWorkBook workbook, string parentId, GraphNode node)
         {
             var thisId = workbook.AddTopic(parentId, node.ObjectType.Name);
 
             foreach (var subNode in node.SubGraph)
             {
-                DrawX(workbook, thisId, subNode);
+                AddXMindTopics(workbook, thisId, subNode);
             }
-
         }
     }
 }
